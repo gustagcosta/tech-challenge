8b48a0774dbdebd00365bf4f28f678af45bfd52c
gustagcosta <gustavoprof3@gmail.com>
2024-07-22 15:01:41 -0300

    add /api to services

--- services/catalog/src/main/java/com/br/catalog/product/ProductController.java
+++ services/catalog/src/main/java/com/br/catalog/product/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
--- services/order/src/app.js
+++ services/order/src/app.js
@@ -7,17 +7,20 @@ import { callbackController } from './routes/callback.js';
 import { getOrderController } from './routes/get-order.js';
 
 const app = express();
+const apiRouter = express.Router();
 
 app.use(cors());
 app.use(express.json());
 
+apiRouter.post('/order', createOrderController);
+apiRouter.post('/order/:order_id/pay', payOrderControler);
+apiRouter.post('/order/callback', callbackController);
+apiRouter.get('/order/:order_id', getOrderController);
+
+app.use('/api', apiRouter);
+
 app.get('/', (req, res) => {
   return res.json({ ok: 'cool' });
 });
-
-app.post('/order', createOrderController);
-app.post('/order/:order_id/pay', payOrderControler);
-app.post('/order/callback', callbackController);
-app.get('/order/:order_id', getOrderController);
 
 app.listen(8083, () => console.log('running on port 8083'));
